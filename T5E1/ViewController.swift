a7f652131a07bfb0933dc99b7a6114d017f02ead
Jorge Marquez Torres <jmarquez@jorges-mac.local>
2016-01-27 13:02:31 +0100

    Tema 5 Ejercicio 1

--- T5E1/ViewController.swift
+++ T5E1/ViewController.swift
@@ -8,8 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+    
+    var popover : UIPopoverController? = nil
+    @IBOutlet weak var buttonHelp: UIButton!
+    @IBAction func buttonHelpAction(sender: AnyObject) {
+        let contenidoPopover : UIViewController = UIViewController()
+        contenidoPopover.view.backgroundColor = UIColor.redColor()
+        contenidoPopover.modalPresentationStyle =
+            UIModalPresentationStyle.Popover
+        let popover = contenidoPopover.popoverPresentationController
+            as UIPopoverPresentationController?
+        contenidoPopover.preferredContentSize = CGSizeMake(300, 300)
+        popover?.delegate = self
+        popover?.sourceView = sender as! UIButton
+        popover?.permittedArrowDirections = UIPopoverArrowDirection.Up
+        popover?.sourceRect = CGRectMake(10, 20, 0, 0)
+        self.presentViewController(contenidoPopover, animated: true,
+            completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
