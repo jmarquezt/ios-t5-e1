01095a0da133bae3fea53866ec9d7fe9ad536965
Jorge Marquez Torres <jmarquez@jorges-mac.local>
2016-01-28 16:10:38 +0100

    Ampliación 1: Mostrar un TableViewController en el Popover en lugar de un rectángulo rojo cutre xD

--- T5E1/ViewController.swift
+++ T5E1/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController, UIPopoverPresentationControllerDelegate 
     var popover : UIPopoverController? = nil
     @IBOutlet weak var buttonHelp: UIButton!
     @IBAction func buttonHelpAction(sender: AnyObject) {
-        let contenidoPopover : UIViewController = UIViewController()
+        /* para ejercicio 1 */
+        /*let contenidoPopover : UIViewController = UIViewController()
         contenidoPopover.view.backgroundColor = UIColor.redColor()
         contenidoPopover.modalPresentationStyle =
             UIModalPresentationStyle.Popover
@@ -25,7 +26,24 @@ class ViewController: UIViewController, UIPopoverPresentationControllerDelegate 
         popover?.permittedArrowDirections = UIPopoverArrowDirection.Up
         popover?.sourceRect = CGRectMake(10, 20, 0, 0)
         self.presentViewController(contenidoPopover, animated: true,
+            completion: nil)*/
+        
+        /* ampliacion */
+        let storyboard = UIStoryboard(name: "Main", bundle: NSBundle.mainBundle())
+        let viewController:UITableViewController = storyboard.instantiateViewControllerWithIdentifier("tableView") as! UITableViewController
+        
+        viewController.modalPresentationStyle =
+            UIModalPresentationStyle.Popover
+        let popover = viewController.popoverPresentationController
+            as UIPopoverPresentationController?
+        viewController.preferredContentSize = CGSizeMake(300, 300)
+        popover?.delegate = self
+        popover?.sourceView = sender as! UIButton
+        popover?.permittedArrowDirections = UIPopoverArrowDirection.Up
+        popover?.sourceRect = CGRectMake(10, 20, 0, 0)
+        self.presentViewController(viewController, animated: true,
             completion: nil)
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
